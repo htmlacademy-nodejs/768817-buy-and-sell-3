7ca5572942795faf531904d9dda9255797e39a02
dmitriev_sergey <info@davra.cz>
2020-03-12 23:56:13 +0100

    module2-task1

--- src/service/cli/version.js
+++ src/service/cli/version.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const packageJson = require(`../../../package.json`);
-
+const chalk = require(`chalk`);
 
 module.exports = {
   name: `--version`,
   run() {
     const version = packageJson.version;
-    console.info(version);
+    console.info(chalk.blue(version));
   }
 };
